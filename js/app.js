276c5f0a4aeb21d7d93a55ab0080c2b3c4b1ff5c
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-07 22:52:54 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 console.log("Welcome to notes app. This is app.js");
 
 // If user adds a note, add it to the localStorage
+
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
