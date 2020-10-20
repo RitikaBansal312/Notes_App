86449c6f3a9fe9c87ec4596e770a9e2da3c46727
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-20 23:55:09 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ addBtn.addEventListener("click", function (e) {
 })
 
 // Function to show elements from localStorage
+
 function showNotes() {
   let notes = localStorage.getItem("notes");
   if (notes == null) {
